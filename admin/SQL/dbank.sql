59270df346952017852d7683f4cc8362dc6036a2
issa-dalloul <issadalloul8@gmail.com>
2024-04-12 18:40:07 +0300

    Updated dbank.sql

--- admin/SQL/dbank.sql
+++ admin/SQL/dbank.sql
@@ -1,9 +1,10 @@
+
 -- phpMyAdmin SQL Dump
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 17, 2024 at 12:28 AM
+-- Generation Time: Jan 19, 2024 at 04:46 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -28,12 +29,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `account` (
-  `acnumber` varchar(6) NOT NULL,
-  `custid` varchar(6) DEFAULT NULL,
-  `bid` varchar(6) DEFAULT NULL,
-  `opening_balance` int(7) DEFAULT NULL,
+  `acnumber` int(11) NOT NULL,
+  `custid` int(11) DEFAULT NULL,
+  `bid` int(11) DEFAULT NULL,
+  `opening_balance` decimal(10,2) DEFAULT NULL,
   `aod` date DEFAULT NULL,
-  `atype` varchar(10) DEFAULT NULL,
+  `atype` varchar(50) DEFAULT NULL,
   `astatus` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -42,29 +43,54 @@ CREATE TABLE `account` (
 --
 
 INSERT INTO `account` (`acnumber`, `custid`, `bid`, `opening_balance`, `aod`, `atype`, `astatus`) VALUES
-('A00001', 'C00001', 'B00001', 1000, '2012-12-15', 'Saving', 'Active'),
-('A00002', 'C00002', 'B00001', 1000, '2012-06-12', 'Saving', 'Active'),
-('A00003', 'C00003', 'B00002', 1000, '2012-05-17', 'Saving', 'Active'),
-('A00004', 'C00002', 'B00005', 1000, '2013-01-27', 'Saving', 'Active'),
-('A00005', 'C00006', 'B00006', 1000, '2012-12-17', 'Saving', 'Active'),
-('A00006', 'C00007', 'B00007', 1000, '2010-08-12', 'Saving', 'Suspended'),
-('A00007', 'C00007', 'B00001', 1000, '2012-10-02', 'Saving', 'Active'),
-('A00008', 'C00001', 'B00003', 1000, '2009-11-09', 'Saving', 'Terminated'),
-('A00009', 'C00003', 'B00007', 1000, '2008-11-30', 'Saving', 'Terminated'),
-('A00010', 'C00004', 'B00002', 1000, '2013-03-01', 'Saving', 'Active');
+(1, 11, 101, 1000.00, '2022-01-01', 'Savings', 'Active'),
+(2, 5, 101, 1000.00, '2022-01-01', 'Savings', 'Active'),
+(3, 7, 101, 1500.00, '2022-01-15', 'Checking', 'Active');
 
 --
 -- Triggers `account`
 --
 DELIMITER $$
-CREATE TRIGGER `tr_prevent_negative_balances` BEFORE UPDATE ON `account` FOR EACH ROW BEGIN
+CREATE TRIGGER `tr_check_non_negative_balance` BEFORE INSERT ON `account` FOR EACH ROW BEGIN
     IF NEW.opening_balance < 0 THEN
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Account balance cannot go negative.';
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Opening balance cannot be negative';
     END IF;
 END
 $$
 DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `tr_prevent_duplicate_accounts` BEFORE INSERT ON `account` FOR EACH ROW BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM account
+        WHERE custid = NEW.custid
+        AND bid = NEW.bid
+    ) THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Customer already has an account with the specified branch.';
+    END IF;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `adminid` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`adminid`, `username`, `password`, `email`) VALUES
+(3, 'ossama', '123', 'newadmin@example.com');
 
 -- --------------------------------------------------------
 
@@ -96,11 +122,25 @@ INSERT INTO `branch` (`bid`, `bname`, `bcity`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `contact_messages`
+--
+
+CREATE TABLE `contact_messages` (
+  `message_id` int(11) NOT NULL,
+  `user_name` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `message_text` text NOT NULL,
+  `submission_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `customer`
 --
 
 CREATE TABLE `customer` (
-  `custid` varchar(6) NOT NULL,
+  `custid` int(11) NOT NULL,
   `fname` varchar(30) DEFAULT NULL,
   `mname` varchar(30) DEFAULT NULL,
   `ltname` varchar(30) DEFAULT NULL,
@@ -115,16 +155,7 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`custid`, `fname`, `mname`, `ltname`, `city`, `mobileno`, `occupation`, `dob`) VALUES
-('C00001', 'Ramesh', 'Chandra', 'Sharma', 'Delhi', '9543198345', 'Service', '1976-12-06'),
-('C00002', 'Avinash', 'Sunder', 'Minha', 'Delhi', '9876532109', 'Service', '1974-10-16'),
-('C00003', 'Rahul', NULL, 'Rastogi', 'Delhi', '9765178901', 'Student', '1981-09-26'),
-('C00004', 'Parul', NULL, 'Gandhi', 'Delhi', '9876532109', 'Housewife', '1976-11-03'),
-('C00005', 'Naveen', 'Chandra', 'Aedekar', 'Mumbai', '8976523190', 'Service', '1976-09-19'),
-('C00006', 'Chitresh', NULL, 'Barwe', 'Mumbai', '7651298321', 'Student', '1992-11-06'),
-('C00007', 'Amit', 'Kumar', 'Borkar', 'Mumbai', '9875189761', 'Student', '1981-09-06'),
-('C00008', 'Nisha', NULL, 'Damle', 'Mumbai', '7954198761', 'Service', '1975-12-03'),
-('C00009', 'Abhishek', NULL, 'Dutta', 'Kolkata', '9856198761', 'Service', '1973-05-22'),
-('C00010', 'Shankar', NULL, 'Nair', 'Chennai', '8765489076', 'Service', '1976-07-12');
+(7, 'issa', 'ziad', 'dalloul', 'beirut', '71067246', 'software e', '2003-06-02');
 
 -- --------------------------------------------------------
 
@@ -143,12 +174,7 @@ CREATE TABLE `loan` (
 --
 
 INSERT INTO `loan` (`custid`, `bid`, `loan_amount`) VALUES
-('C00001', 'B00001', 100000),
-('C00002', 'B00002', 200000),
-('C00009', 'B00008', 400000),
-('C00010', 'B00009', 500000),
-('C00001', 'B00003', 600000),
-('C00002', 'B00001', 600000);
+('7', '101', 2000);
 
 --
 -- Triggers `loan`
@@ -169,13 +195,26 @@ CREATE TRIGGER `tr_prevent_duplicate_loan_requests` BEFORE INSERT ON `loan` FOR 
         WHERE custid = NEW.custid
         AND bid = NEW.bid
         AND loan_amount > 0
-        AND astatus = 'Pending'
+       
     ) THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Customer already has a pending loan request with the specified branch.';
     END IF;
 END
 $$
 DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `loan_applications`
+--
+
+CREATE TABLE `loan_applications` (
+  `application_id` int(11) NOT NULL,
+  `custid` int(11) DEFAULT NULL,
+  `amount` decimal(10,2) DEFAULT NULL,
+  `status` enum('pending','accepted','rejected') DEFAULT 'pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -197,18 +236,69 @@ CREATE TABLE `trandetails` (
 --
 
 INSERT INTO `trandetails` (`tnumber`, `acnumber`, `dot`, `medium_of_transaction`, `transaction_type`, `transaction_amount`) VALUES
-('T00001', 'A00001', '2013-01-01', 'Cheque', 'Deposit', 2000),
-('T00002', 'A00001', '2013-02-01', 'Cash', 'Withdrawal', 1000),
-('T00003', 'A00002', '2013-01-01', 'Cash', 'Deposit', 2000),
-('T00004', 'A00002', '2013-02-01', 'Cash', 'Deposit', 3000),
-('T00005', 'A00007', '2013-01-11', 'Cash', 'Deposit', 7000),
-('T00006', 'A00007', '2013-01-13', 'Cash', 'Deposit', 9000),
-('T00007', 'A00001', '2013-03-13', 'Cash', 'Deposit', 4000),
-('T00008', 'A00001', '2013-03-14', 'Cheque', 'Deposit', 3000),
-('T00009', 'A00001', '2013-03-21', 'Cash', 'Withdrawal', 9000),
-('T00010', 'A00001', '2013-03-22', 'Cash', 'Withdrawal', 2000),
-('T00011', 'A00002', '2013-03-25', 'Cash', 'Withdrawal', 7000),
-('T00012', 'A00007', '2013-03-26', 'Cash', 'Withdrawal', 2000);
+('', '3', '2022-01-15', 'Online', 'Withdrawal', 200);
+
+--
+-- Triggers `trandetails`
+--
+DELIMITER $$
+CREATE TRIGGER `tr_check_non_negative_transaction_amount` BEFORE INSERT ON `trandetails` FOR EACH ROW BEGIN
+    IF NEW.transaction_amount < 0 THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Transaction amount cannot be negative';
+    END IF;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `tr_check_transaction_amount_vs_balance` BEFORE INSERT ON `trandetails` FOR EACH ROW BEGIN
+    DECLARE current_balance DECIMAL(10, 2);
+    SELECT opening_balance INTO current_balance FROM account WHERE acnumber = NEW.acnumber;
+
+    IF NEW.transaction_amount > current_balance THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Transaction amount exceeds the account balance';
+    END IF;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `userid` int(11) NOT NULL,
+  `custid` int(11) DEFAULT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `user_type` enum('customer','admin') DEFAULT 'customer'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userid`, `custid`, `username`, `password`, `email`, `user_type`) VALUES
+(0, 7, 'issa', '123', 'issadalloul262003@gmail.com', 'customer'),
+(0, NULL, 'ossama', '123', 'newadmin@example.com', 'admin');
+
+--
+-- Triggers `users`
+--
+DELIMITER $$
+CREATE TRIGGER `tr_check_unique_username` BEFORE INSERT ON `users` FOR EACH ROW BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM users
+        WHERE username = NEW.username
+    ) THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Username must be unique';
+    END IF;
+END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -233,10 +323,10 @@ CREATE TABLE `vw_loan_details` (
 -- (See below for the actual view)
 --
 CREATE TABLE `vw_total_balance_per_customer` (
-`custid` varchar(6)
+`custid` int(11)
 ,`fname` varchar(30)
 ,`ltname` varchar(30)
-,`total_balance` decimal(32,0)
+,`total_balance` decimal(32,2)
 );
 
 -- --------------------------------------------------------
@@ -265,15 +355,25 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 -- Indexes for table `account`
 --
 ALTER TABLE `account`
-  ADD PRIMARY KEY (`acnumber`),
-  ADD KEY `idx_account_custid` (`custid`),
-  ADD KEY `idx_account_bid` (`bid`);
+  ADD PRIMARY KEY (`acnumber`);
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`adminid`);
 
 --
 -- Indexes for table `branch`
 --
 ALTER TABLE `branch`
   ADD PRIMARY KEY (`bid`);
+
+--
+-- Indexes for table `contact_messages`
+--
+ALTER TABLE `contact_messages`
+  ADD PRIMARY KEY (`message_id`);
 
 --
 -- Indexes for table `customer`
@@ -292,6 +392,12 @@ ALTER TABLE `loan`
   ADD KEY `idx_loan_loan_amount` (`loan_amount`);
 
 --
+-- Indexes for table `loan_applications`
+--
+ALTER TABLE `loan_applications`
+  ADD PRIMARY KEY (`application_id`);
+
+--
 -- Indexes for table `trandetails`
 --
 ALTER TABLE `trandetails`
@@ -299,28 +405,38 @@ ALTER TABLE `trandetails`
   ADD KEY `idx_trandetails_acnumber` (`acnumber`);
 
 --
--- Constraints for dumped tables
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- Constraints for table `account`
+-- AUTO_INCREMENT for table `account`
 --
 ALTER TABLE `account`
-  ADD CONSTRAINT `account_bid_fk` FOREIGN KEY (`bid`) REFERENCES `branch` (`bid`),
-  ADD CONSTRAINT `account_custid_fk` FOREIGN KEY (`custid`) REFERENCES `customer` (`custid`);
+  MODIFY `acnumber` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- Constraints for table `loan`
+-- AUTO_INCREMENT for table `admin`
 --
-ALTER TABLE `loan`
-  ADD CONSTRAINT `loan_bid_fk` FOREIGN KEY (`bid`) REFERENCES `branch` (`bid`),
-  ADD CONSTRAINT `loan_custid_fk` FOREIGN KEY (`custid`) REFERENCES `customer` (`custid`);
+ALTER TABLE `admin`
+  MODIFY `adminid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- Constraints for table `trandetails`
+-- AUTO_INCREMENT for table `contact_messages`
 --
-ALTER TABLE `trandetails`
-  ADD CONSTRAINT `trandetails_acnumber_fk` FOREIGN KEY (`acnumber`) REFERENCES `account` (`acnumber`);
+ALTER TABLE `contact_messages`
+  MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `custid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `loan_applications`
+--
+ALTER TABLE `loan_applications`
+  MODIFY `application_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
